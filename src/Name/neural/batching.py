fix broadcasting issues, add missing entry encoder

src/Name/neural/batching.py
@@ -1,5 +1,3 @@
-from ..data.tokenization import TokenizedFile, TokenizedAST
-
 from typing import NamedTuple, Iterator, TypeVar
 
 import torch
@@ -10,6 +8,8 @@ from torch.nn.utils.rnn import pad_sequence
 from math import ceil
 from random import sample
 from itertools import groupby
+
+from ..data.tokenization import TokenizedFile, TokenizedAST
 
 
 class BatchedASTs(NamedTuple):
@@ -69,14 +69,14 @@ class Collator:
                         for i, (scope_len, hole_count) in enumerate(zip(scope_lens, hole_counts))]
         target_index = [torch.arange((start := i * most_holes), start + hole_count).repeat_interleave(scope_len)
                         for i, (scope_len, hole_count) in enumerate(zip(scope_lens, hole_counts))]
-        edge_index = torch.stack((torch.cat(source_index), torch.cat(target_index)))
+        edge_index = torch.stack((torch.cat(source_index), torch.cat(target_index))).to(self.cast_to)
 
         # dense padded batches
         scope_types: Tensor = self.pad_arrays(scope_types)
         scope_definitions: Tensor = self.pad_arrays(scope_definitions)
         hole_types: Tensor = self.pad_arrays(hole_types)
         lemmas: Tensor = torch.tensor(
-            [i in ps for scope, holes in files for _, _, ps in holes for i in range(len(scope))],
+            [i in ps for _, scope, holes in files for _, _, ps in holes for i in range(len(scope))],
             dtype=torch.bool, device=self.cast_to)
 
         # reference offsets
@@ -91,20 +91,19 @@ class Collator:
         scope_type_ref_values = scope_type_offsets[scope_type_ref_mask]
         scope_def_ref_values = scope_def_offsets[scope_def_ref_mask]
         hole_type_ref_values = hole_type_offsets[hole_type_ref_mask]
-
         return Batch(scope_types=
-                     BatchedASTs(tokens=scope_types,
-                                 token_mask=self.make_token_mask(scope_types),
+                     BatchedASTs(tokens=scope_types.permute(3, 0, 1, 2),
+                                 token_mask=self.make_token_mask(scope_types).flatten(0, 1),
                                  reference_mask=scope_type_ref_mask,
                                  reference_ids=scope_type_ref_values),
                      scope_definitions=
-                     BatchedASTs(tokens=scope_definitions,
-                                 token_mask=self.make_token_mask(scope_definitions),
+                     BatchedASTs(tokens=scope_definitions.permute(3, 0, 1, 2),
+                                 token_mask=self.make_token_mask(scope_definitions).flatten(0, 1),
                                  reference_mask=scope_def_ref_mask,
                                  reference_ids=scope_def_ref_values),
                      hole_types=
-                     BatchedASTs(tokens=hole_types,
-                                 token_mask=self.make_token_mask(hole_types),
+                     BatchedASTs(tokens=hole_types.permute(3, 0, 1, 2),
+                                 token_mask=self.make_token_mask(hole_types).flatten(0, 1),
                                  reference_mask=hole_type_ref_mask,
                                  reference_ids=hole_type_ref_values),
                      batch_pts=batch_pts,
@@ -167,5 +166,5 @@ class Sampler:
             batch_indices = [(scope_id, [hole for _, holes in items for hole in holes])
                              for scope_id, items
                              in groupby(sorted(batch_indices, key=lambda x: x[0]), key=lambda x: x[0])]
-            yield [(self.data[scope_idx][1], select(self.data[scope_idx][2], hole_ids))
+            yield [(self.data[scope_idx][0], self.data[scope_idx][1], select(self.data[scope_idx][2], hole_ids))
                    for scope_idx, hole_ids in batch_indices]

src/Name/neural/embedding.py
@@ -70,7 +70,7 @@ class TokenEmbedding(Module):
         self.dim = dim
         self.path_encoder = BinaryPathEncoder.orthogonal(dim // 2)
         self.db_encoder = SequentialPositionEncoder(dim // 4, freq=max_db_index)
-        self.embeddings = Embedding(num_embeddings=12, embedding_dim=dim // 4)
+        self.embeddings = Embedding(num_embeddings=12, embedding_dim=dim // 2)
         """
         Embedding map:
             0 [SOS]
@@ -110,10 +110,12 @@ class TokenEmbedding(Module):
         content_embeddings[bop_mask] = self.embeddings.forward(token_values[bop_mask] + 1)
         content_embeddings[nop_mask] = self.embeddings.forward(token_values[nop_mask] + 5)
         content_embeddings[ref_mask] = self.embeddings.weight[8]
+        db_values = self.db_encoder.forward(token_values[db_mask])
         content_embeddings[db_mask] = torch.cat((
-            self.embeddings.weight[9][:self.dim//4], self.db_encoder.forward(token_values[db_mask])), dim=-1)
+            self.embeddings.weight[9][:self.dim//4].expand_as(db_values), db_values), dim=-1)
+        var_values = self.db_encoder.forward(token_values[var_mask])
         content_embeddings[var_mask] = torch.cat((
-            self.embeddings.weight[9][self.dim // 4:], self.db_encoder.forward(token_values[var_mask])), dim=-1)
+            self.embeddings.weight[9][self.dim // 4:].expand_as(var_values), var_values), dim=-1)
         content_embeddings[oos_mask] = self.embeddings.weight[10]
 
         # if lm_mask is not None:

src/Name/neural/encoders.py
@@ -1,7 +1,8 @@
-from .utils.modules import EncoderLayer, ResidualFFN
-from torch.nn import Module, ModuleList
+from torch.nn import Module, ModuleList, Bilinear
 from torch import Tensor
 from torch.distributions import Gumbel
+
+from .utils.modules import EncoderLayer, ResidualFFN
 
 
 class ReferenceUpdater(Module):
@@ -15,7 +16,7 @@ class ReferenceUpdater(Module):
                 reference_ids: Tensor,
                 reference_embeddings: Tensor) -> Tensor:
         gate = token_embeddings[reference_mask]
-        ctx = reference_embeddings[reference_ids]
+        ctx = reference_embeddings.flatten(0, 1)[reference_ids]
         update = self.fusion_nn.forward(x=ctx, gate=gate)
         token_embeddings[reference_mask] = update
         return token_embeddings
@@ -51,7 +52,7 @@ class EntryEncoderLayer(Module):
         encoder_kwargs = {'num_heads': num_heads, 'dim': dim, 'atn_dim': atn_dim, 'dropout_rate': dropout_rate}
         self.type_encoder = TermEncoderLayer(**encoder_kwargs)
         self.def_encoder = self.type_encoder if self.share_params else TermEncoderLayer(**encoder_kwargs)
-        self.entry_encoder = ...
+        self.entry_encoder = Bilinear(dim, dim, dim, bias=False)
 
     def forward(self,
                 type_token_embeddings: Tensor,
@@ -75,7 +76,7 @@ class EntryEncoderLayer(Module):
             reference_mask=def_token_ref_mask,
             reference_ids=def_token_ref_ids,
             reference_embeddings=reference_embeddings)
-        entry_embeddings = self.entry_encoder(type_token_embeddings[:, 0], def_token_embeddings[:, 0])
+        entry_embeddings = self.entry_encoder(type_token_embeddings[:, :, 0], def_token_embeddings[:, :, 0])
         return entry_embeddings, type_token_embeddings, def_token_embeddings
 
 

src/Name/neural/train.py
@@ -1,10 +1,10 @@
-from .model import ModelCfg, Model
-from .batching import Batch
-
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LRScheduler
 
 from typing import TypedDict, Iterator
+
+from .model import ModelCfg, Model
+from .batching import Batch
 
 
 class TrainCfg(TypedDict):
@@ -26,8 +26,24 @@ class TrainCfg(TypedDict):
 
 class TrainStats(TypedDict):
     loss:               float
-    predictions:        list[int]
-    truth:              list[int]
+    predictions:        list[bool]
+    truth:              list[bool]
+
+
+def binary_stats(predictions: list[bool], truths: list[bool]) -> tuple[int, int, int, int]:
+    tp = sum([x == y for x, y in zip(predictions, truths) if y])
+    fn = sum([x != y for x, y in zip(predictions, truths) if y])
+    tn = sum([x == y for x, y in zip(predictions, truths) if not y])
+    fp = sum([x != y for x, y in zip(predictions, truths) if not y])
+    return tp, fn, tn, fp
+
+
+def macro_binary_stats(tp: int, fn: int, tn: int, fp: int) -> tuple[float, float, float, float]:
+    prec = tp / (tp + fp + 1e-08)
+    rec = tp / (tp + fn + 1e-08)
+    f1 = 2 * prec * rec / (prec + rec + 1e-08)
+    accuracy = (tp + tn) / (tp + fn + tn + fp)
+    return accuracy, f1, prec, rec
 
 
 class Trainer(Model):

src/Name/neural/utils/modules.py
@@ -2,6 +2,7 @@ import torch
 from torch import Tensor
 from torch.nn import Module, Parameter, Linear, Dropout
 from torch.nn.functional import binary_cross_entropy_with_logits
+
 from .attention import lin_atn_fn, atn_fn
 
 
@@ -96,7 +97,7 @@ class ResidualFFN(Module):
         self.ffn = SwiGLU(dim, intermediate, dim)
         self.dropout = Dropout(dropout_rate)
 
-    def forward(self, x: Tensor, gate: Tensor | None) -> Tensor:
+    def forward(self, x: Tensor, gate: Tensor | None = None) -> Tensor:
         norm = self.pre_norm(x)
         ffn = self.ffn(norm, gate)
         return ffn + norm