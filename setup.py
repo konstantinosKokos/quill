6bb0227c4e43ff707cb57ca9f8cfecde30a6367f
konstantinosKokos <konstantinos@riseup.net>
2025-01-07 15:14:05 +0100

    add setuptools

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agda-quill',
+    version='0.1.0',
+    author='Konstantinos Kogkalidis',
+    description='Neural premise selection for Agda.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/konstantinosKokos/quill',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'torch>=2.4',
+        'torch-geometric>=2.6',
+        'flask>=2.2',
+        'requests'
+    ],
+    python_requires='>=3.11',
+)
